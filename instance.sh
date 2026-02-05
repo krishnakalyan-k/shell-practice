be2d1def017ff2daf458e28df84a38747191d99a
kalyan k <kalyansanstorage@gmail.com>
2026-02-06 00:44:11 +0530

    script pushing

---
+++ instance.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SG_ID="sg-0b9e5f45b94d933fa" # replace with your ID
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z05013202FKF0ZL12WAOP"
+DOMAIN_NAME="daws88s.online"
+
+for instance in $@
+do
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
