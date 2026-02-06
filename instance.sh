f1d15e27b17cd4e040e18c7ec90ea67c3f28a67f
kalyan k <kalyansanstorage@gmail.com>
2026-02-06 22:12:51 +0530

    script pushing

--- instance.sh
+++ instance.sh
@@ -2,18 +2,11 @@
 
 SG_ID="sg-0b9e5f45b94d933fa" # replace with your ID
 AMI_ID="ami-0220d79f3f480ecf5"
-ZONE_ID="Z05013202FKF0ZL12WAOP"
-DOMAIN_NAME="daws88s.online"
 
-for instance in $@
-do
-    INSTANCE_ID=$( aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type "t3.micro" \
-    --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    --query 'Instances[0].InstanceId' \
-    --output text )
-    done
+INSTANCE_ID=$( aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t3.micro --security-group-ids $SG_ID)
 
-    echo "$INSTANCE_ID"
+if [ $? -eq 0 ]; then
+echo "Instance created:) instance ID=$INSTANCE_ID"
+else 
+echo "Instance creation failed"
+fi
