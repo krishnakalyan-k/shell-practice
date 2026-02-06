e74f1084cad8f09843cf711be829adaab1a21aea
kalyan k <kalyansanstorage@gmail.com>
2026-02-06 23:10:34 +0530

    script pushing

--- instance.sh
+++ instance.sh
@@ -3,10 +3,11 @@
 SG_ID="sg-0b9e5f45b94d933fa" # replace with your ID
 AMI_ID="ami-0220d79f3f480ecf5"
 
-INSTANCE_ID=$( aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=DB,Value=DEMO}]' --query 'Instances[0].InstanceId,IP:PublicIpAddress' --output text)
+INSTANCE_ID=$( aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t3.micro --security-group-ids $SG_ID --query 'Instances[0].InstanceId' --output text)
+
 if [ $? -eq 0 ]; then
 echo "Instance created:) instanceID=$INSTANCE_ID"
-echo "PrivateIP:) $IP"
 else 
 echo "Instance creation failed"
 fi
+
