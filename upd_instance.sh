7d17a05b7fecb46389899b6c04c0d9e512ed1081
kalyan k <kalyansanstorage@gmail.com>
2026-02-13 19:56:54 +0530

    adding

---
+++ upd_instance.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SG_ID="sg-0b9e5f45b94d933fa" # replace with your ID
+AMI_ID="ami-0220d79f3f480ecf5"
+FILE="instance_name.txt"
+
+for instance in $(cat $FILE)
+
+do
+
+INSTANCE_ID=$( aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$FILE}]" --query 'Instances[0].InstanceId' --output text)
+
+if [ $? -eq 0 ]; then
+echo "Instance created:) instanceID=$INSTANCE_ID"
+else 
+echo "Instance creation failed"
+
+fi
+
+if [ "$instance" == "frontend" ]; then
+
+publicIP=$( aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+echo "$instance PUBLIC_IP:) $publicIP"
+
+else
+ privateIP=$( aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+echo "$instance PRIVATE_IP:) $privateIP"
+
+fi 
+
+done
