820b00932e8ea851e51b336d331e949282db2e34
kalyan k <kalyansanstorage@gmail.com>
2026-02-16 00:59:29 +0530

    adding

--- roboshoptest1.sh
+++ roboshoptest1.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
-USERID=$(id -u)
-LOGS_FOLDER="/var/log/shell-roboshop"
-LOGS_FILE="$LOGS_FOLDER/$0.log"
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+
 INSTANCE_INFO="instances_53recordsinfo.txt"
+FILE="instance_name.txt"
 
-if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
-    exit 1
-fi
-
-mkdir -p $LOGS_FOLDER
-
-VALIDATE(){
-    if [ $1 -ne 0 ]; then
-        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
-    fi
-}
 
 FILE="instance_name.txt"
 
@@ -35,14 +15,21 @@ mongoprivate_ip=$(awk '$1=="mongodb" {print $2}' "$INSTANCE_INFO")
 mongopublic_ip=$(awk '$1=="mongodb" {print $3}' "$INSTANCE_INFO")
 mongodns_name=$(awk '$1=="mongodb" {print $4}' "$INSTANCE_INFO")
 
-ssh $mongoprivate_ip <<EOF
+ssh $mongopublic_ip <<EOF
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" 
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" 
+    fi
+}
+
 VALIDATE $? "Lonin to mongodb"
 
-sudo su -
-$pwd
-
 git clone https://github.com/krishnakalyan-k/shell-practice.git
-
+VALIDATE $? "git clone"
 cp /root/shell-practice/mongoDB_verinfo.txt /etc/yum.repos.d/mongo.repo
 VALIDATE $? "copying of mongodb verinfo file"
 
@@ -60,7 +47,7 @@ VALIDATE $? "MongoDB services started"
 EOF
 
 else 
-echo "mongodb details not found in the file"
+echo "instance not found in the file"
 fi
 
 done
