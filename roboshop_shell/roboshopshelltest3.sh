04e215be677bddbec422c67c172fd2075745c422
kalyan k <kalyansanstorage@gmail.com>
2026-02-18 14:52:35 +0530

    automation

--- roboshop_shell/roboshopshelltest3.sh
+++ roboshop_shell/roboshopshelltest3.sh
@@ -56,7 +56,7 @@ private_ip=$(awk -v inst="$instance" '$1==inst {print $2}' "$INSTANCE_INFO")
 public_ip=$(awk -v inst="$instance" '$1==inst {print $3}' "$INSTANCE_INFO")
 dns_name=$(awk -v inst="$instance" '$1==inst {print $4}' "$INSTANCE_INFO")
 
-ssh  root@$public_ip << EOF
+ssh  root@$public_ip <<'EOF'
 VALIDATE(){
     if [ $1 -ne 0 ]; then
         echo -e "$2 ... $R FAILURE $N" 
