1a324db1a214e128f727410a8b6a148c38eef8b7
kalyan k <kalyansanstorage@gmail.com>
2026-02-18 16:50:02 +0530

    automation

--- roboshoptest.sh
+++ roboshoptest.sh
@@ -25,7 +25,7 @@ VALIDATE $? "Connected to MongoDB server"
 
 rm -rf /root/shell-practice
 rm -rf /etc/yum.repos.d/mongo.repo
-
+dnf remove mongodb-org -y
 
 git clone https://github.com/krishnakalyan-k/shell-practice.git
 VALIDATE $? "git clone"
